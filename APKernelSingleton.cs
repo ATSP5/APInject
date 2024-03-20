001d560d19eb427ea6b2f647abb7117f81509b0f
Adam Prukała <adamprukala@gmail.com>
2024-03-20 21:31:08 +0100

    Added singleton container

---
+++ APKernelSingleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APInject
+{
+    public class APKernelSingleton
+    {
+        public static APInjectKernel Kernel { get; private set; }
+
+        private static APKernelSingleton _instance;
+
+        private static readonly object _lock = new object();
+
+        public static void RebuildKernel(APInjectLoader loader) 
+        {
+            Kernel = new APInjectKernel(loader);
+        }
+        private APKernelSingleton(APInjectLoader loader) 
+        {
+            Kernel = new APInjectKernel(loader);
+        }
+        public static APKernelSingleton GetInstance(APInjectLoader loader)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+
+                    if (_instance == null)
+                    {
+                        _instance = new APKernelSingleton(loader);
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
